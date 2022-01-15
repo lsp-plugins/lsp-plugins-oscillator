68be415147a5f97c158ef145c558e42c631a832b
sadko4u <sadko4u@gmail.com>
2022-01-16 01:43:03 +0300

    Updated build scripts

--- plugins.mk
+++ plugins.mk
@@ -18,13 +18,6 @@
 # along with lsp-plugins-oscillator.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-# Package version
-ARTIFACT_ID                 = LSP_PLUGINS_OSCILLATOR
-ARTIFACT_NAME               = lsp-plugins-oscillator
-ARTIFACT_TYPE               = plug
-ARTIFACT_DESC               = LSP Oscillator Plugin Series
-ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.1-devel
-
-
-
+# Variables that describe dependencies
+PLUGIN_DEPENDENCIES     = LSP_PLUGINS_OSCILLATOR
+PLUGIN_PACKAGE_NAME     = lsp-plugins
