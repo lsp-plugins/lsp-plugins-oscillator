b08b5827ba0912bbd3cdb74006c3d78e2404d74d
sadko4u <sadko4u@gmail.com>
2022-09-06 19:46:32 +0300

    Preparing for release

--- project.mk
+++ project.mk
@@ -24,7 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-oscillator
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Oscillator Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.4-devel
+ARTIFACT_VERSION            = 1.0.4
 
 
 
