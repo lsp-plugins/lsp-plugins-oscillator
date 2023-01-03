62fcf3745005f8e45ee3ad8a4440dbef0f57ba3f
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-01-03 19:49:08 +0300

    Updated build scripts and dependencies

--- project.mk
+++ project.mk
@@ -24,6 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-oscillator
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Oscillator Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
+ARTIFACT_EXPORT_HEADERS     = 0
 ARTIFACT_VERSION            = 1.0.6-devel
 
 
